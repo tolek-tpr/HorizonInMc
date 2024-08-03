66f98a3ba4505ef7a68e2860060f25b824f82457
Tolek Rudnicki <tolek@epsi.pl>
2024-08-03 18:43:16 +0200

    mixin stuff

---
+++ src/main/java/pl/epsi/mixin/client/ExampleMixin.java
@@ -0,0 +1,11 @@
+package pl.epsi.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Environment(EnvType.CLIENT)
+@Mixin(MinecraftClient.class)
+public class ExampleMixin {
+}
