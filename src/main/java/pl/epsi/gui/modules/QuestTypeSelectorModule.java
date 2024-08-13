153869f6d480d79da0265f8274a3d83965e556f3
Michał Niedźwiedzki <michal@royaltiz.com>
2024-08-13 19:52:29 +0200

    Added cursors to text renderer

--- src/main/java/pl/epsi/gui/modules/QuestTypeSelectorModule.java
+++ src/main/java/pl/epsi/gui/modules/QuestTypeSelectorModule.java
@@ -61,79 +61,79 @@ public class QuestTypeSelectorModule extends ContainerWidget {
 
         if (this.drawNames) {
             if (iv.questEntrySelected == 0) {
-                renderer.renderLine(context, "ACTIVE", x + 40, j, 1, 10);
+                renderer.renderLine(context, "ACTIVE", x + 40, j);
             } else {
-                renderer.renderLine(context, "ACTIVE", x + 30, j, 1, 10);
+                renderer.renderLine(context, "ACTIVE", x + 30, j);
             }
             if (iv.questEntrySelected == 1) {
-                renderer.renderLine(context, "MAIN", x + 40, j + 30, 1, 10);
+                renderer.renderLine(context, "MAIN", x + 40, j + 30);
             } else {
-                renderer.renderLine(context, "MAIN", x + 30, j + 30, 1, 10);
+                renderer.renderLine(context, "MAIN", x + 30, j + 30);
             }
             if (iv.questEntrySelected == 2) {
-                renderer.renderLine(context, "SIDE", x + 40, j + 55, 1, 10);
+                renderer.renderLine(context, "SIDE", x + 40, j + 55);
             } else {
-                renderer.renderLine(context, "SIDE", x + 30, j + 55, 1, 10);
+                renderer.renderLine(context, "SIDE", x + 30, j + 55);
             }
             if (iv.questEntrySelected == 3) {
-                renderer.renderLine(context, "ERRANDS", x + 40, j + 80, 1, 10);
+                renderer.renderLine(context, "ERRANDS", x + 40, j + 80);
             } else {
-                renderer.renderLine(context, "ERRANDS", x + 30, j + 80, 1, 10);
+                renderer.renderLine(context, "ERRANDS", x + 30, j + 80);
             }
             if (iv.questEntrySelected == 4) {
-                renderer.renderLine(context, "JOBS", x + 40, j + 105, 1, 10);
+                renderer.renderLine(context, "JOBS", x + 40, j + 105);
             } else {
-                renderer.renderLine(context, "JOBS", x + 30, j + 105, 1, 10);
+                renderer.renderLine(context, "JOBS", x + 30, j + 105);
             }
             if (iv.questEntrySelected == 5) {
-                renderer.renderLine(context, "SALVAGE CONTRACTS", x + 40, j + 130, 1, 10);
+                renderer.renderLine(context, "SALVAGE CONTRACTS", x + 40, j + 130);
             } else {
-                renderer.renderLine(context, "SALVAGE CONTRACTS", x + 30, j + 130, 1, 10);
+                renderer.renderLine(context, "SALVAGE CONTRACTS", x + 30, j + 130);
             }
             if (iv.questEntrySelected == 6) {
-                renderer.renderLine(context, "HUNTING GROUNDS", x + 40, j + 155, 1, 10);
+                renderer.renderLine(context, "HUNTING GROUNDS", x + 40, j + 155);
             } else {
-                renderer.renderLine(context, "HUNTING GROUNDS", x + 30, j + 155, 1, 10);
+                renderer.renderLine(context, "HUNTING GROUNDS", x + 30, j + 155);
             }
             if (iv.questEntrySelected == 7) {
-                renderer.renderLine(context, "MELEE PIT", x + 40, j + 180, 1, 10);
+                renderer.renderLine(context, "MELEE PIT", x + 40, j + 180);
             } else {
-                renderer.renderLine(context, "MELEE PIT", x + 30, j + 180, 1, 10);
+                renderer.renderLine(context, "MELEE PIT", x + 30, j + 180);
             }
             if (iv.questEntrySelected == 8) {
-                renderer.renderLine(context, "THE ARENA", x + 40, j + 205, 1, 10);
+                renderer.renderLine(context, "THE ARENA", x + 40, j + 205);
             } else {
-                renderer.renderLine(context, "THE ARENA", x + 30, j + 205, 1, 10);
+                renderer.renderLine(context, "THE ARENA", x + 30, j + 205);
             }
             if (iv.questEntrySelected == 9) {
-                renderer.renderLine(context, "REBEL CAMPS", x + 40, j + 230, 1, 10);
+                renderer.renderLine(context, "REBEL CAMPS", x + 40, j + 230);
             } else {
-                renderer.renderLine(context, "REBEL CAMPS", x + 30, j + 230, 1, 10);
+                renderer.renderLine(context, "REBEL CAMPS", x + 30, j + 230);
             }
             if (iv.questEntrySelected == 10) {
-                renderer.renderLine(context, "REBEL OUTPOSTS", x + 40, j + 255, 1, 10);
+                renderer.renderLine(context, "REBEL OUTPOSTS", x + 40, j + 255);
             } else {
-                renderer.renderLine(context, "REBEL OUTPOSTS", x + 30, j + 255, 1, 10);
+                renderer.renderLine(context, "REBEL OUTPOSTS", x + 30, j + 255);
             }
             if (iv.questEntrySelected == 11) {
-                renderer.renderLine(context, "RELIC RUINS", x + 40, j + 280, 1, 10);
+                renderer.renderLine(context, "RELIC RUINS", x + 40, j + 280);
             } else {
-                renderer.renderLine(context, "RELIC RUINS", x + 30, j + 280, 1, 10);
+                renderer.renderLine(context, "RELIC RUINS", x + 30, j + 280);
             }
             if (iv.questEntrySelected == 12) {
-                renderer.renderLine(context, "TALLNECKS", x + 40, j + 305, 1, 10);
+                renderer.renderLine(context, "TALLNECKS", x + 40, j + 305);
             } else {
-                renderer.renderLine(context, "TALLNECKS", x + 30, j + 305, 1, 10);
+                renderer.renderLine(context, "TALLNECKS", x + 30, j + 305);
             }
             if (iv.questEntrySelected == 13) {
-                renderer.renderLine(context, "CAULDRONS", x + 40, j + 330, 1, 10);
+                renderer.renderLine(context, "CAULDRONS", x + 40, j + 330);
             } else {
-                renderer.renderLine(context, "CAULDRONS", x + 30, j + 330, 1, 10);
+                renderer.renderLine(context, "CAULDRONS", x + 30, j + 330);
             }
             if (iv.questEntrySelected == 14) {
-                renderer.renderLine(context, "COLLECTABLES", x + 40, j + 355, 1, 10);
+                renderer.renderLine(context, "COLLECTABLES", x + 40, j + 355);
             } else {
-                renderer.renderLine(context, "COLLECTABLES", x + 30, j + 355, 1, 10);
+                renderer.renderLine(context, "COLLECTABLES", x + 30, j + 355);
             }
         }
     }
--- src/main/java/pl/epsi/render/CustomTextRenderer.java
+++ src/main/java/pl/epsi/render/CustomTextRenderer.java
@@ -2,25 +2,24 @@ package pl.epsi.render;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.util.Identifier;
+import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class CustomTextRenderer {
 
-    public static class CustomTextRendererException extends Exception {
-        CustomTextRendererException(String msg) {
-            super(msg);
-        }
-    }
-
     public static class FontProps {
         private String name;
         private int bitmapHeight = 16;
         private int bitmapWidth = 232;
-        private int baseLine = 12;
+        private int baseLine = 10;
+        private int baseHeight = 10;
         private int fallbackWidth = 16;
         private int fallbackHeight = 10;
         private final Map<Character, ArrayList<Integer>> characters = new TreeMap<>();
@@ -33,13 +32,42 @@ public class CustomTextRenderer {
 
     private int horizontalSpacing = 1;
     private int verticalSpacing = 1;
-    private int scaling = 1;
 
     public enum HorizontalAlign { LEFT, RIGHT, CENTER }
     private HorizontalAlign horizontalAlign = HorizontalAlign.LEFT;
 
     public enum VerticalAlign { TOP, BOTTOM, CENTER }
     private VerticalAlign verticalAlign = VerticalAlign.TOP;
+
+    public class ItemToRender {
+        public final int x;
+        public final int y;
+        public final int width;
+        public final int height;
+        public final int posX;
+        public final int posY;
+
+        public ItemToRender(int x, int y, int width, int height, int posX, int posY) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.posX = posX;
+            this.posY = posY;
+        }
+    }
+
+    public class Cursor {
+        public final int x;
+        public final int y;
+        public final int count;
+
+        public Cursor(int x, int y, int count) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
+    }
 
     private CustomTextRenderer(Identifier font, FontProps props) {
         this.font = font;
@@ -48,13 +76,16 @@ public class CustomTextRenderer {
 
     public static CustomTextRenderer of(String modId, String fontName) {
         Identifier font = new Identifier(modId, fontName + ".png");
-//        if (!renderers.containsKey(font)) {
-            Yaml yaml = new Yaml();
-            final FontProps props = (FontProps) yaml.load(fontName + ".yml");
-//            renderers.put(font, new CustomTextRenderer(font, props));
-//        }
-//        return renderers.get(font);
-    return new CustomTextRenderer(font, props);
+        if (!renderers.containsKey(font)) {
+            Yaml yaml = new Yaml(new Constructor(FontProps.class, new LoaderOptions()));
+            try {
+                FontProps props = yaml.load(new FileInputStream(fontName + ".yml"));
+                renderers.put(font, new CustomTextRenderer(font, props));
+            } catch (Exception e) {
+                System.err.println("Font file '" + fontName ".yml' not found");
+            }
+        }
+        return renderers.get(font);
     }
 
     public CustomTextRenderer setHorizontalSpacing(int horizontalSpacing) {
@@ -67,11 +98,6 @@ public class CustomTextRenderer {
         return this;
     }
 
-    public CustomTextRenderer setScaling(int scaling) {
-        this.scaling = scaling;
-        return this;
-    }
-
     public CustomTextRenderer setHorizontalAlign(HorizontalAlign horizontalAlign) {
         this.horizontalAlign = horizontalAlign;
         return this;
@@ -81,22 +107,55 @@ public class CustomTextRenderer {
         this.verticalAlign = verticalAlign;
         return this;
     }
-    public int renderLine(DrawContext context, String text, int x, int y) throws CustomTextRendererException {
-        int offset = 0;
+
+    public int renderChar(DrawContext context, char ch, int x, int y) {
+        return renderChar(context, ch, new Cursor(x, y, 0)).count;
+    }
+
+    public Cursor renderChar(DrawContext context, char ch, Cursor cursor) {
+        if (!props.characters.containsKey(ch)) return cursor;
+        ArrayList<Integer> position = props.characters.get(ch);
+        int posX = position.get(0);
+        int posY = position.get(1);
+        int w = position.get(2) == null ? props.fallbackWidth : position.get(2);
+        int h = position.get(3) == null ? props.fallbackHeight : position.get(3);
+        context.drawTexture(font, cursor.x, cursor.y, w, h,
+                posX, posY, w,
+                h, props.bitmapWidth, props.bitmapHeight);
+        return new Cursor(cursor.x + w + horizontalSpacing, cursor.y, cursor.count + 1);
+    }
+
+    public int renderLine(DrawContext context, String text, int x, int y) {
+        return renderLine(context, text, new Cursor(x, y, 0)).count;
+    }
+
+    public Cursor renderLine(DrawContext context, String text, Cursor cursor) {
+        for (int i = 0; i < text.length(); ++i) {
+            cursor = renderChar(context, text.charAt(i), cursor);
+        }
+        return cursor;
+    }
+
+    public int renderRegion(DrawContext context, String text, int x, int y, int width, int height) {
+        return renderRegion(context, text, new Cursor(x, y, 0), width, height).count;
+    }
+
+    public Cursor renderRegion(DrawContext context, String text, Cursor initialCursor, int width, int height) {
+        Cursor cursor = new Cursor(initialCursor.x, initialCursor.y, initialCursor.count);
         for (int i = 0; i < text.length(); ++i) {
             final char ch = text.charAt(i);
-            if (!props.characters.containsKey(ch)) throw new CustomTextRendererException("Character '" + ch + "' not found in font map");
-            final ArrayList<Integer> position = props.characters.get(ch);
-            final int posX = position.get(0);
-            final int posY = position.get(1);
-            final int width = position.get(2) == null ? props.fallbackWidth : position.get(2);
-            final int height = position.get(3) == null ? props.fallbackHeight : position.get(3);
-            context.drawTexture(font, x + offset, y, width, height,
-                    posX, posY, width,
-                    height, props.bitmapWidth, props.bitmapHeight);
-            offset += Math.ceil((width + horizontalSpacing) * scaling);
+            if (ch == '\n' || cursor.x > initialCursor.x + width) {
+                cursor = new Cursor(cursor.x, cursor.y + props.baseHeight + verticalSpacing, cursor.count);
+                if (ch == '\n') continue;
+            }
+            if (cursor.x == initialCursor.x && ch == ' ') {
+                cursor = new Cursor(cursor.x, cursor.y, cursor.count + 1);
+                continue;
+            }
+            if (cursor.y > initialCursor.y + height) return cursor;
+            cursor = renderChar(context, ch, cursor);
         }
-        return offset;
+        return cursor;
     }
 
 }
