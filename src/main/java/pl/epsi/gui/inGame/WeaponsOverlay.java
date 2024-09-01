2ff26a3152c52c2ca1bc4eeae3e0a4712c056a44
Tolek Rudnicki <tolek@epsi.pl>
2024-09-01 16:46:10 +0200

    Fixed up readme, added file saving, started on weapon overlay

--- src/main/java/pl/epsi/gui/inGame/WeaponsOverlay.java
+++ src/main/java/pl/epsi/gui/inGame/WeaponsOverlay.java
@@ -1,13 +1,19 @@
 package pl.epsi.gui.inGame;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
+import pl.epsi.HorizonInMc;
 import pl.epsi.event.EventImpl;
 import pl.epsi.event.EventManager;
 import pl.epsi.event.HudRenderCallbackListener;
 import pl.epsi.event.UpdateListener;
+import pl.epsi.render.HorizonShader;
+
+import java.nio.file.Path;
+import java.util.Optional;
 
 public class WeaponsOverlay extends EventImpl implements HudRenderCallbackListener, UpdateListener {
 
@@ -21,6 +27,7 @@ public class WeaponsOverlay extends EventImpl implements HudRenderCallbackListen
     }
 
     public boolean shouldRender = false;
+    public boolean shaderLoaded = false;
 
     @Override
     public void onEnable() {
@@ -36,7 +43,18 @@ public class WeaponsOverlay extends EventImpl implements HudRenderCallbackListen
 
     @Override
     public void onRender(DrawContext context, float tickDelta) {
-        if (!this.shouldRender) return;
+        if (!shaderLoaded) {
+            Optional<Path> o = FabricLoader.getInstance().getModContainer(HorizonInMc.MOD_ID).get()
+                    .findPath("assets/" + HorizonInMc.MOD_ID + "/shaders/core/someVertexShader.vsh");
+            Optional<Path> o1 = FabricLoader.getInstance().getModContainer(HorizonInMc.MOD_ID).get()
+                    .findPath("assets/" + HorizonInMc.MOD_ID + "/shaders/core/someFragShader.fsh");
+
+            if (o.isPresent() && o1.isPresent()) {
+                HorizonShader shader = new HorizonShader(o.get().toFile(), o1.get().toFile());
+                shader.use();
+            }
+        }
+        //if (!this.shouldRender) return;
         context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "TEST", 500, 500, 0xffffff);
     }
 
